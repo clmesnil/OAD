5ae8bf78650a2ee59fb4d7a7dec265a0a7ccf319
clmesnil <50318338+clmesnil@users.noreply.github.com>
2019-11-05 18:27:09 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include "Jobshop.hpp"
+
+using namespace std;
+
+
+int main() {
+	srand(time(0));
+	T_instance instance;
+	T_solution sol;
+	lire_fichier("test.txt", instance); //fonctionnel
+	afficher_instance(instance); //fonctionnel
+	creer_prime(instance); //fonctionnel
+	
+	//generer_vecteur_alea(sol, instance);
+	//copie(V1, sol.V);
+	//afficher_vecteur(sol, instance);
+	evaluer(sol, instance);
+
+	return 1;
+}
